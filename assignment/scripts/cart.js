37ed2f9cf7399f7ba167dabf1dc607dd22147365
Fue Lee <fueclee.12@gmail.com>
2023-02-08 16:46:40 -0600

    wrote out first versions of the functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,20 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem( item ) {
+    basket.push( item );
+    return true;
+}
+
+function listItems( array ) {
+    for( item of array ) {
+        console.log( item );
+    }
+}
+
+function empty( array ) {
+    array = [];
+    return array;
+}
